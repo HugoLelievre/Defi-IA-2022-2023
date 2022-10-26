5007e47248260daf0be8528ed7c0fcfbef02bcb2
HugoLelievre <98543148+HugoLelievre@users.noreply.github.com>
2022-10-26 12:39:19 +0200

    Add files via upload

---
+++ include meta data.py
@@ -0,0 +1,14 @@
+# %%
+import numpy as np
+import pandas as pd
+pricing_requests = pd.read_csv("pricing_requests.csv")
+features_hotels = pd.read_csv("features_hotels.csv")
+
+# %%
+merged_df = pricing_requests.merge(features_hotels, on = ["hotel_id","city"])
+merged_df.to_csv('merged_df.csv', index=False)
+df = merged_df[["price","stock","city","date","language","mobile",
+                "request_number", "group","brand","parking",
+                "pool","children_policy"]].copy()
+df.to_csv('df.csv', index=False)
+# %%
