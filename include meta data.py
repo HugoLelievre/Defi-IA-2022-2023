6a6be0ae5d8bbd392f93f297c7ffaafd81b10d01
HugoLelievre <98543148+HugoLelievre@users.noreply.github.com>
2022-11-01 15:49:49 +0100

    Add files via upload

--- include meta data.py
+++ include meta data.py
@@ -7,7 +7,7 @@ features_hotels = pd.read_csv("features_hotels.csv")
 # %%
 merged_df = pricing_requests.merge(features_hotels, on = ["hotel_id","city"])
 merged_df.to_csv('merged_df.csv', index=False)
-df = merged_df[["price","stock","hotel_id","city","date","language","mobile",
+df = merged_df[["price","stock","city","date","language","mobile",
                 "request_number", "group","brand","parking",
                 "pool","children_policy"]].copy()
 df.to_csv('df.csv', index=False)
