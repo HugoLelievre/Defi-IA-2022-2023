8ae9cd40a5387350fd2829fba2b169cdb5ec726e
HugoLelievre <98543148+HugoLelievre@users.noreply.github.com>
2022-11-03 12:33:08 +0100

    Add files via upload

--- include meta data.py
+++ include meta data.py
@@ -10,5 +10,60 @@ merged_df.to_csv('merged_df.csv', index=False)
 df = merged_df[["price","stock","city","date","language","mobile",
                 "request_number", "group","brand","parking",
                 "pool","children_policy"]].copy()
-df.to_csv('df.csv', index=False)
 # %%
+list_cities = ["amsterdam", "copenhagen", "madrid", "paris", "rome", "sofia", "valletta", "vienna", "vilnius"]
+list_cities_price = []
+for city in list_cities:
+    sub_df = df[(df.request_number == 1) & (df.city == city)]
+    list_cities_price.append(np.mean(sub_df.price))
+
+df.city = pd.Categorical(df.city)
+df["price_city"] = df.city.cat.rename_categories(list_cities_price)
+
+list_brands = df.brand.unique()
+list_brands_price = []
+for brand in list_brands:
+    sub_df = df[(df.request_number == 1) & (df.brand == brand)]
+    list_brands_price.append(np.mean(sub_df.price))
+
+df.brand = pd.Categorical(df.brand)
+df["price_brand"] = df.brand.cat.rename_categories(list_brands_price)
+
+list_groups = df.group.unique()
+list_groups_price = []
+for group in list_groups:
+    sub_df = df[(df.request_number == 1) & (df.group == group)]
+    list_groups_price.append(np.mean(sub_df.price))
+
+df.group = pd.Categorical(df.group)
+df["price_group"] = df.group.cat.rename_categories(list_groups_price)
+# %%
+df.drop(['brand','group','city','language'],axis=1).to_csv('df.csv', index=False)
+# %%
+def TransformTestSample(test_sample):
+    res = test_sample.copy()
+    res["request_number"] = np.NaN
+    res["request_id"] = res["city"] + res["language"] + res["avatar_id"].astype(str)
+    for k in range(1,max(res["order_requests"]) + 1):
+        rq_id = res.loc[res["order_requests"]==k,"request_id"].unique()[0]
+        max_rq = np.nanmax(res.loc[(res.request_id==rq_id) & (res.order_requests<=k),"request_number"])
+        if np.isnan(max_rq):
+            max_rq = 0
+        res.loc[(res.request_id==rq_id) & (res.order_requests==k),"request_number"] = max_rq + 1
+    return(res.drop(['request_id', 'index', 'order_requests','avatar_id'], axis=1))
+# %%
+test_sample = pd.read_csv("test_set.csv")
+df_test = TransformTestSample(test_sample).drop('language',axis=1)
+features_hotels = pd.read_csv("features_hotels.csv")
+merged_df_test = df_test.merge(features_hotels, on = ["hotel_id","city"]).drop('hotel_id', axis=1)
+
+# %%
+merged_df_test.city = pd.Categorical(merged_df_test.city)
+merged_df_test["price_city"] = merged_df_test.city.cat.rename_categories(list_cities_price)
+merged_df_test.brand = pd.Categorical(merged_df_test.brand)
+merged_df_test["price_brand"] = merged_df_test.brand.cat.rename_categories(list_brands_price)
+merged_df_test.group = pd.Categorical(merged_df_test.group)
+merged_df_test["price_group"] = merged_df_test.group.cat.rename_categories(list_groups_price)
+# %%
+merged_df_test.drop(['brand','group','city'],axis=1).to_csv("final_test_df.csv", index=False)
+
--- submission file.py
+++ submission file.py
@@ -6,71 +6,21 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.neural_network import MLPRegressor
 df = pd.read_csv("df.csv")
-test_sample = pd.read_csv("test_set.csv")
+test_sample = pd.read_csv("final_test_df.csv")
 # %%
-bdd_dummies = pd.get_dummies(data=df, columns = ['group','brand','city', 'language'], drop_first=True)
-X_train = bdd_dummies.loc[:,bdd_dummies.columns != "price"].copy()
-Y_train = bdd_dummies.loc[:,"price"].copy()
+X_train = df.loc[:,df.columns != "price"].copy()
+Y_train = df.loc[:,"price"].copy()
 
 # %%
-def TransformTestSample(test_sample):
-    res = test_sample.copy()
-    res["request_number"] = np.NaN
-    res["request_id"] = res["city"] + res["language"] + res["avatar_id"].astype(str)
-    for k in range(1,max(res["order_requests"]) + 1):
-        rq_id = res.loc[res["order_requests"]==k,"request_id"].unique()[0]
-        max_rq = np.nanmax(res.loc[(res.request_id==rq_id) & (res.order_requests<=k),"request_number"])
-        if np.isnan(max_rq):
-            max_rq = 0
-        res.loc[(res.request_id==rq_id) & (res.order_requests==k),"request_number"] = max_rq + 1
-    return(res.drop(['request_id', 'index', 'order_requests','avatar_id'], axis=1))
-
-# %%
-df_test = TransformTestSample(test_sample)
-features_hotels = pd.read_csv("features_hotels.csv")
-merged_df_test = df_test.merge(features_hotels, on = ["hotel_id","city"]).drop('hotel_id', axis=1)
-df_test_dummies = pd.get_dummies(data=merged_df_test, columns = ['group','brand','city', 'language'], drop_first=True)
-# col_names = X_train.columns[np.logical_not(X_train.columns.isin(df_test_dummies.columns))]
-# for name in col_names:
-#     print(name)
-#     df_test_dummies[name] = 0
-
-# %%
-rf = RandomForestRegressor().fit(X_train, Y_train)
-y_pred_rf = rf.predict(df_test_dummies)
-submission = pd.DataFrame({"index" : df_test_dummies.index, "price" : y_pred_rf})
-submission.to_csv('submission.csv', index=False)
-
-# %%
-mlp = MLPRegressor().fit(X_train, Y_train)
-y_pred_mlp = mlp.predict(df_test_dummies)
-submission = pd.DataFrame({"index" : df_test_dummies.index, "price" : y_pred_mlp})
+rf = RandomForestRegressor(n_estimators=400, 
+                           min_samples_split=10,
+                           min_samples_leaf=2,
+                           max_features="sqrt",
+                           max_depth=15,
+                           bootstrap=False).fit(X_train.drop(['price_group','parking','mobile','request_number'],axis=1), Y_train)
+y_pred_rf = rf.predict(test_sample.drop(['price_group','parking','mobile','request_number'],axis=1))
+submission = pd.DataFrame({"index" : test_sample.index, "price" : y_pred_rf})
 submission.to_csv('submission.csv', index=False)
 # %%
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-# Number of features to consider at every split
-max_features = ['auto', 'sqrt']
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
-# Create the random grid
-random_grid = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
-print(random_grid)
-# %%
-rf = RandomForestRegressor()
-rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
-rf_random.fit(X_train, Y_train)
-
+pd.DataFrame({"variable":X_train.columns, "importance":rf.feature_importances_}).sort_values("importance", ascending=False)
 # %%
